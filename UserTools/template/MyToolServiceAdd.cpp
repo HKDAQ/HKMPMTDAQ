635774f5538e7cf65ffa6de44c4d323c8dbc2151
brichards64 <b.richards@qmul.ac.uk>
2019-12-11 13:44:47 +0100

    added service add template

---
+++ UserTools/template/MyToolServiceAdd.cpp
@@ -0,0 +1,46 @@
+#include "MyToolServiceAdd.h"
+
+MyToolServiceAdd::MyToolServiceAdd():Tool(){}
+
+
+bool MyToolServiceAdd::Initialise(std::string configfile, DataModel &data){
+
+  if(configfile!="")  m_variables.Initialise(configfile);
+  //m_variables.Print();
+
+  m_data= &data;
+
+  if(!m_variables.Get("Port",m_port)) m_port=5555;
+
+  m_util=new Utilities(m_data->context);
+  
+  sock = new  zmq::socket_t(*(m_data->context), ZMQ_DEALER);
+
+  std::stringstream tmp;
+  tmp<<"tcp://*:"<<m_port;
+
+  sock->bind(tmp.str().c_str());
+
+  if (!m_util->AddService("MyService",m_port,false)) return false;
+  
+  return true;
+}
+
+
+bool MyToolServiceAdd::Execute(){
+
+  return true;
+}
+
+
+bool MyToolServiceAdd::Finalise(){
+
+  bool ret=m_util->RemoveService("MyService");
+
+  delete sock;
+  sock=0;
+
+  if(!ret) return false;
+
+  return true;
+}
